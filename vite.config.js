605c23e6705b3a240c79e90fabe086ea36d47963
kevinlixx <113084403+kevinlixx@users.noreply.github.com>
2024-11-16 21:37:39 -0500

    intento2

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/<proyectoidx2>/', // Reemplaza <nombre-del-repositorio> con el nombre de tu repositorio
+  base: '/proyectoidx2/', // Reemplaza 'proyectoidx2' con el nombre de tu repositorio
   plugins: [react()],
-  server: {
-    headers: {
-      'Permissions-Policy': 'interest-cohort=()' // Elimina o corrige esta cabecera si es necesario
-    }
-  }
 });
