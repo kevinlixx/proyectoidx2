04b56a969f8422aefea4d314d4bdf02c7fc9ef70
kevinlixx <113084403+kevinlixx@users.noreply.github.com>
2024-11-16 21:20:32 -0500

    intento1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/<proyectoidx2>/', // Reemplaza <nombre-del-repositorio> con el nombre de tu repositorio
   plugins: [react()],
-})
+});
